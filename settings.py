855ea1ad54560fa82b4c0ac5c69eb65a6683b4ab
Ben Morris <bcmorri4@ncsu.edu>
2024-03-01 10:14:58 -0500

    Added importing assests to settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # COLORS (r, g, b)
-from re import S
-
+import pygame
+import os
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -22,3 +22,14 @@ WIDTH = TILESIZE * COLS
 HEIGHT = TILESIZE * ROWS
 FPS = 60
 TITLE = "Minesweeper Clone"
+
+tile_numbers = []
+for i in range(1, 9):
+  tile_numbers.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Tile{i}.png")), (TILESIZE, TILESIZE)))
+
+tile_empty = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileEmpty.png")), (TILESIZE, TILESIZE))
+tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileExploded.png")), (TILESIZE, TILESIZE))
+tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileFlag.png")), (TILESIZE, TILESIZE))
+tile_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileMine.png")), (TILESIZE, TILESIZE))
+tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileUnknown.png")), (TILESIZE, TILESIZE))
+tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileNotMine.png")), (TILESIZE, TILESIZE))
