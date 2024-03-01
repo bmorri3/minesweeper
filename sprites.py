52434ca5a8427737bffb306c65307213aad7316c
Ben Morris <bcmorri4@ncsu.edu>
2024-03-01 12:34:16 -0500

    Implemented showing unkown tile when not revealed and board tile when revealed.

--- sprites.py
+++ sprites.py
@@ -21,7 +21,13 @@ class Tile:
       return self.type
 
   def draw(self, board_surface):
-    board_surface.blit(self.image, (self.x, self.y))
+    if not self.flagged and self.revealed:      
+      board_surface.blit(self.image, (self.x, self.y))
+    elif self.flagged and not self.revealed:
+      board_surface.blit(tile_flag, (self.x, self.y))
+    elif not self.revealed:
+      board_surface.blit(tile_unknown, (self.x, self.y))
+
 
 
 class Board:
@@ -29,6 +35,7 @@ class Board:
     self.board_surface = pygame.Surface((WIDTH, HEIGHT))
     self.board_list = [[Tile(col, row, tile_empty, ".") for row in range(ROWS)] for col in range(COLS)]
     self.place_mines()
+    self.place_clues()
     
   def place_mines(self):
     for _ in range(NUM_MINES):
@@ -36,20 +43,41 @@ class Board:
         x = random.randint(0, COLS - 1)
         y = random.randint(0, ROWS - 1)
         
-        if self.board_list[x][x].type == ".":
+        if self.board_list[x][x].image is tile_empty:
           self.board_list[x][y].image = tile_mine
           self.board_list[x][y].type = "X"
           break
-        
-	def place_clues(self):
-		pass
 
-	@staticmethod
-  def is_inside(self, x, y):
+  def place_clues(self):
+    for col in range(COLS):
+      for row in range(ROWS):        
+        if self.board_list[col][row].image is tile_empty:
+          total_mines = self.check_neighbors(col, row)
+          if total_mines != 0:
+            self.board_list[col][row].image = tile_numbers[total_mines - 1]
+            self.board_list[col][row].type = "C"
+
+
+  @staticmethod
+  def is_inside(x, y):
      return 0 <= x < ROWS and 0 <= y < COLS
 
-	def check_neighbors(self, x, y):
-    for 
+  def check_neighbors(self, x, y):
+    total_mines = 0
+    
+    for col in range(x-1, x+2):
+      for row in range(y-1, y+2):
+        
+        if self.is_inside(col, row) and self.board_list[col][row].image == tile_mine:
+            total_mines += 1
+    
+    # This is in the event that the central square is a mine. This should never be called as we are only checking
+    # neighbors if a tile is empty
+    if self.board_list[x][y].image == tile_mine:
+      total_mines -= 1
+
+    return total_mines
+        
    
   def display_board(self):
     for i in self.board_list:
