ce9554bd8e49938aa3589692cf5210477f1623a9
Ben Morris <bcmorri4@ncsu.edu>
2024-03-01 08:51:04 -0500

    Intial main, sprites, and settings uploaded

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+# Minesweeper using Pygame
+# From tutorial: https://www.youtube.com/watch?v=n0jZRlhLtt0&ab_channel=Tech%26Gaming
 
+import pygame
+from settings import *
+from sprites import *
 
+class Game:
+    def __init__ (self):
+        self.screen = pygame.display.set_mode(WIDTH, HEIGHT)
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
 
+    def new(self):
+        pass
+    
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.draw()
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+game = Game()
+while True:
+    game.new()
+    game.run()
